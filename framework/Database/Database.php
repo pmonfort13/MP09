e0bc81e6ea1582b2a788aa7ebdd19845377c2a6d
pmonfort13 <pmonfort@iesebre.com>
2023-11-06 19:35:47 +0100

    Sync

---
+++ framework/Database/Database.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Framework\Database;
+
+use PDO;
+
+class Database
+{
+    protected $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo ? $pdo;
+    }
+
+    public function selectAll($table)
+    {
+        $statement = $this->pdo->prepare("SELECT * FROM $table;");
+    }
+}
